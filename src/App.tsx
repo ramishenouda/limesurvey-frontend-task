1b40a50d13a3308e47ab131d74643761250c68be
ramishenouda <ramishenouda@outlook.com>
2023-03-08 13:06:54 +0200

    feat: moved the app logic to the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { SurveyEditor } from './components/SurveyEditor';
-import { Survey } from './shared/interfaces/Survey/Survey.interface';
-import { getAllSurveys } from './services/survey.service';
 import { SurveyDashboard } from './components/SurveyDashboard';
 
 function App() {
-  const [survey, setSurvey] = useState<Survey>();
-  const [userSurveys, setUserSurveys] = useState<Array<Survey>>();
-
-  useEffect(() => {
-    getAllSurveys()
-      .then((response) => response.json())
-      .then((surveys) => {
-        setUserSurveys(surveys);
-      });
-  }, []);
-
   return (
     <div className="mb-2">
-      <div className="w-full p-4 text-4xl bg-blue-400 navbar">
+      <div className="w-full p-4 mb-4 text-4xl bg-blue-400 navbar">
         <span className="p-2 bg-blue-300 rounded-md">GeniusSurvey</span>
       </div>
-      {!userSurveys && <p className="mt-4 text-4xl text-center">Loading...</p>}
-      <div className="p-4">
-        {userSurveys && !survey && (
-          <SurveyDashboard setUserSurveys={setUserSurveys} setSurvey={setSurvey} surveys={userSurveys} />
-        )}
-      </div>
-      <div>{survey && <SurveyEditor survey={survey} setSurvey={setSurvey} />}</div>
+      <SurveyDashboard />
     </div>
   );
 }
--- src/components/SurveyDashboard.tsx
+++ src/components/SurveyDashboard.tsx
@@ -1,27 +1,39 @@
+import { useEffect, useState } from 'react';
+import { getAllSurveys } from '../services/survey.service';
 import { Survey } from '../shared/interfaces/Survey/Survey.interface';
+import { SurveyEditor } from './SurveyEditor';
 
-type Props = {
-  surveys: Array<Survey>;
-  setSurvey: React.Dispatch<React.SetStateAction<Survey | undefined>>;
-  setUserSurveys: React.Dispatch<React.SetStateAction<Survey[] | undefined>>;
-};
+export const SurveyDashboard = () => {
+  const [survey, setSurvey] = useState<Survey>();
+  const [userSurveys, setUserSurveys] = useState<Array<Survey>>();
 
-export const SurveyDashboard = ({ surveys, setUserSurveys, setSurvey }: Props) => {
+  useEffect(() => {
+    getAllSurveys()
+      .then((response) => response.json())
+      .then((surveys) => {
+        setUserSurveys(surveys);
+      });
+  }, []);
+
   return (
     <>
-      <div className="flex flex-wrap items-center justify-center gap-8 p-4 mx-auto border w-fit">
-        {surveys.map((survey) => {
-          return (
-            <div
-              onClick={() => setSurvey(survey)}
-              className="flex items-center justify-center h-40 text-2xl transition-all duration-500 bg-blue-200 border rounded-md cursor-pointer hover:bg-blue-300 w-80 sm:text-4xl sm:w-96"
-              key={survey.title + 'survey'}
-            >
-              {survey.title}
-            </div>
-          );
-        })}
-      </div>
+      {!userSurveys && <p className="mt-4 text-4xl text-center">Loading...</p>}
+      {userSurveys && !survey && (
+        <div className="flex flex-wrap items-center justify-center gap-8 p-4 mx-auto border w-fit">
+          {userSurveys.map((survey) => {
+            return (
+              <div
+                onClick={() => setSurvey(survey)}
+                className="flex items-center justify-center h-40 text-2xl transition-all duration-500 bg-blue-200 border rounded-md cursor-pointer hover:bg-blue-300 w-80 sm:text-4xl sm:w-96"
+                key={survey.title + 'survey'}
+              >
+                {survey.title}
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div>{survey && <SurveyEditor survey={survey} setSurvey={setSurvey} />}</div>
     </>
   );
 };
